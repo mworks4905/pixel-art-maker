018342a1f43eb00360529f6d70faadc24530ddf6
Matt Works <mworks4905@gmail.com>
2016-08-12 19:48:10 -0600

    update work

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ console.log('doc is loaded!')
 
 console.log("i'm connected!");
 
+function createGrid(grid){
+  for(i = 0; i < grid.length; i++){
+    var gridSquare = document.createElement("div");
+    var gridMasterDiv = document.getElementsByClassName("grid")
+    gridMasterDiv.appendChild(gridSquare);
+
+  }
+}
+createGrid(10);
 // var btn = document.querySelectorAll('button')[0];
 // console.log('btn: ',btn);
 //
